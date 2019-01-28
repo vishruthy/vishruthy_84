2e9f4f8d4b498aa8dbb1ad82c43836a8e07b8728
vishruthy <vishruthy.k@vvdntech.in>
2019-01-28 17:52:56 +0530

    initial

---
+++ sub.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter two numbers");
+scanf("%d%d", &a, &b);
+c=a+b;
+printf("the addition of two numbers is %d",c);
+c=a-b;
+printf("the sub of two numbers is %d",c);
+
+
+return 0;
+}
+
